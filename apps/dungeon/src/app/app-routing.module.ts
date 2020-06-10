b24aca83af701b037239243bdd3c2fe69c4cbb1a
Darío Scattolini <dario.scattolini@gmail.com>
2020-06-10 17:53:53 +0200

    test<AppComponent>: add router-outlet stub, tests pass for this component

--- apps/dungeon/src/app/app-routing.module.ts
+++ apps/dungeon/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes }  from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { paths } from './routes.paths'
+import { paths } from './routes.paths';
 
 const appRoutes: Routes = [
   {
--- apps/dungeon/src/app/app.component.spec.ts
+++ apps/dungeon/src/app/app.component.spec.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+@Component({selector: 'router-outlet', template: ''})
+class RouterOutletStubComponent { }
+
 describe('AppComponent', () => {
   let app: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent]
+      declarations: [
+        AppComponent,
+        RouterOutletStubComponent
+      ]
     }).compileComponents();
   }));
 
