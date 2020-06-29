a7d93f16d5181948d43b209154cb4b77e07dddf0
Darío Scattolini <dario.scattolini@gmail.com>
2020-06-29 21:00:03 +0200

    test<Player model>: add constructor specification

--- apps/dungeon/src/app/models/player.spec.ts
+++ apps/dungeon/src/app/models/player.spec.ts
@@ -1,7 +1,13 @@
 import { Player } from './player';
 
 describe('Player', () => {
-  it('should create an instance', () => {
-    expect(new Player()).toBeTruthy();
+  it('should create instance', () => {
+    expect(new Player('John')).toBeTruthy();
   });
+
+  it('should create instance named "John" for that constructor parameter', () => {
+    const player = new Player('John');
+    expect(player.name).toBe('John');
+  })
+
 });
