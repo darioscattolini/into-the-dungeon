97eedd93d374d1739586ffc2f93e4d2cbfa0c58c
Darío Scattolini <dario.scattolini@gmail.com>
2020-06-29 18:44:39 +0200

    feat<one-device>: add services module with GameManager service to OneDeviceModule

---
+++ apps/dungeon/src/app/one-device/services/services.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { GameManagerService } from './game-manager.service';
+
+
+@NgModule({
+  providers: [ GameManagerService ]
+})
+export class ServicesModule { }
