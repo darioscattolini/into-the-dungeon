96c3e618de8922c4be750c8072543228c8af7f9d
Darío Scattolini <dario.scattolini@gmail.com>
2020-06-29 21:14:13 +0200

    test<Player model>: add specification for constructor validation

--- apps/dungeon/src/app/models/player.spec.ts
+++ apps/dungeon/src/app/models/player.spec.ts
@@ -8,6 +8,11 @@ describe('Player', () => {
   it('should create instance named "John" for that constructor parameter', () => {
     const player = new Player('John');
     expect(player.name).toBe('John');
-  })
+  });
+
+  it('should not create a player with empty name', () => {
+    expect(() => { const player = new Player(''); })
+      .toThrow(new Error('Player\'s names should have at least one character'));
+  });
 
 });
