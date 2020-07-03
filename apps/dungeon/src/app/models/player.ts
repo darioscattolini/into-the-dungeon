bd5fb88793f081aa92cbb8cbe1bfe90d029a734a
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-03 20:55:24 +0200

    refactor<PlayerModel>: make fields private

--- apps/dungeon/src/app/models/player.ts
+++ apps/dungeon/src/app/models/player.ts
@@ -1,12 +1,24 @@
 export class Player {
-  public name: string;
-  public victories: number;
-  public defeats: number;
+  private _name: string;
+  private _victories: number;
+  private _defeats: number;
 
   constructor(name: string) { 
     if (name === '') throw new Error('Player\'s names should have at least one character');
-    this.name = name;
-    this.victories = 0;
-    this.defeats = 0;
+    this._name = name;
+    this._victories = 0;
+    this._defeats = 0;
+  }
+
+  public get name() {
+    return this._name;
+  }
+
+  public get victories() {
+    return this._victories;
+  }
+
+  public get defeats() {
+    return this._defeats;
   }
 }
