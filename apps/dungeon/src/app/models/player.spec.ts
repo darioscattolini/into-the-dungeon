2f924e4e97866f4d01e65d6cec4597c8d95e96a6
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-03 20:57:30 +0200

    test<PlayerModel>: refactor spec to separate constructor tests

--- apps/dungeon/src/app/models/player.spec.ts
+++ apps/dungeon/src/app/models/player.spec.ts
@@ -7,24 +7,26 @@ describe('Player', () => {
     player = new Player('John');
   });
 
-  it('should create instance', () => {
-    expect(player).toBeTruthy();
-  });
-
-  it('should create instance named "John" for that constructor parameter', () => {
-    expect(player.name).toBe('John');
-  });
-
-  it('should not create a player with empty name', () => {
-    expect(() => { const unnamedPlayer = new Player(''); })
-      .toThrow(new Error('Player\'s names should have at least one character'));
-  });
+  describe('constructor', () => {
+    it('should create instance', () => {
+      expect(player).toBeTruthy();
+    });
   
-  it('should be created with 0 victories', () => {
-    expect(player.victories).toStrictEqual(0);
-  });
-
-  it('should be created with 0 defeats', () => {
-    expect(player.defeats).toStrictEqual(0);
+    it('should create instance named "John" for that constructor parameter', () => {
+      expect(player.name).toBe('John');
+    });
+  
+    it('should not create a player with empty name', () => {
+      expect(() => { const unnamedPlayer = new Player(''); })
+        .toThrow(new Error('Player\'s names should have at least one character'));
+    });
+    
+    it('should create a player with 0 victories', () => {
+      expect(player.victories).toStrictEqual(0);
+    });
+  
+    it('should create a player with 0 defeats', () => {
+      expect(player.defeats).toStrictEqual(0);
+    });
   });
 });
