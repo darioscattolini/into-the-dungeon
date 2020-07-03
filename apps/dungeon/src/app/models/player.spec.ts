a9255dd69bc894d1ba3c7adc35a154f7e818421c
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-03 20:42:27 +0200

    test<PlayerModel>: refactor spec

--- apps/dungeon/src/app/models/player.spec.ts
+++ apps/dungeon/src/app/models/player.spec.ts
@@ -1,17 +1,23 @@
 import { Player } from './player';
 
 describe('Player', () => {
+  let player: Player;
+
+  beforeEach(() => {
+    player = new Player('John');
+  });
+
   it('should create instance', () => {
-    expect(new Player('John')).toBeTruthy();
+    expect(player).toBeTruthy();
   });
 
   it('should create instance named "John" for that constructor parameter', () => {
-    const player = new Player('John');
     expect(player.name).toBe('John');
   });
 
   it('should not create a player with empty name', () => {
-    expect(() => { const player = new Player(''); })
+
+    expect(() => { const unnamedPlayer = new Player(''); })
       .toThrow(new Error('Player\'s names should have at least one character'));
   });
 });
