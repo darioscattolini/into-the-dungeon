70a0cc37c7a183ce58c29451b0350be614c6906b
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-03 20:47:58 +0200

    feat<PlayerModel>: add defeats property initialized with 0 in constructor

--- apps/dungeon/src/app/models/player.ts
+++ apps/dungeon/src/app/models/player.ts
@@ -1,10 +1,12 @@
 export class Player {
   public name: string;
   public victories: number;
+  public defeats: number;
 
   constructor(name: string) { 
     if (name === '') throw new Error('Player\'s names should have at least one character');
     this.name = name;
     this.victories = 0;
+    this.defeats = 0;
   }
 }
