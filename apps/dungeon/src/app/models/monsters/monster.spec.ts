d422c74000947efcf3758b6052a3a508130e4139
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-07 13:29:52 +0200

    test<MonsterModel>: refine spec for constructor, should assign name to instance

--- apps/dungeon/src/app/models/monsters/monster.spec.ts
+++ apps/dungeon/src/app/models/monsters/monster.spec.ts
@@ -15,5 +15,13 @@ describe('Monster', () => {
     it('should help create an instance of an extension', () => {
       expect(monster).toBeTruthy();
     });
+
+    it('should create an instance of Monster', () => {
+      expect(monster instanceof Monster).toStrictEqual(true);
+    });
+
+    it('should create an instance named "Scary"', () => {
+      expect(monster.name).toStrictEqual('Scary');
+    });
   });
 });
