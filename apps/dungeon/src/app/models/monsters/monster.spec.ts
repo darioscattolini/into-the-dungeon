e4d2bdc3e3a0c12eb51e50123054ccbc470f7680
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-07 21:56:01 +0200

    test<MonsterModel>: refactor spec to simplify configuration and assertions

--- apps/dungeon/src/app/models/monsters/monster.spec.ts
+++ apps/dungeon/src/app/models/monsters/monster.spec.ts
@@ -26,19 +26,23 @@ function buildMonsterPack(opponent: Hero): Monster[] {
 }
 
 describe('Monster', () => {
-  let monster: ConcreteMonsterType1;
   let opponent: Hero;
 
   beforeEach(() => {
     opponent = new class extends Hero {};
-    monster = new ConcreteMonsterType1(opponent);
   });
 
   afterEach(() => {
     Monster.clearUncoveredInstances();
   })
 
-  describe('constructor', () => {
+  describe('single monster tests', () => {
+    let monster: ConcreteMonsterType1;
+
+    beforeEach(() => {
+      monster = new ConcreteMonsterType1(opponent);
+    });
+
     it('should help create an instance of an extension', () => {
       expect(monster).toBeTruthy();
     });
@@ -67,39 +71,45 @@ describe('Monster', () => {
     it('should register hero as monster opponent', () => {
       expect(monster.opponent).toStrictEqual(opponent);
     });
+  });
 
-    it('should register each created monster in static property uncoveredInstances', () => {
-      Monster.clearUncoveredInstances();
-      const monsterPack = buildMonsterPack(opponent);
-      expect(Monster.uncoveredInstances).toEqual(monsterPack);
-    });
+  describe('many monsters tests', () => {
+    let monsterPack: Monster[];
+    let monsterType1_1: Monster,
+        monsterType2_1: Monster,
+        monsterType1_2: Monster,
+        monsterType2_2: Monster,
+        monsterType1_3: Monster;
 
-    it('should register the number of instances of its type in nthOfItsType property', () => {
-      Monster.clearUncoveredInstances();
-      const [
+    beforeEach(() => {
+      monsterPack = buildMonsterPack(opponent);
+      [
         monsterType1_1,
         monsterType2_1,
         monsterType1_2,
         monsterType2_2,
         monsterType1_3
-      ] = buildMonsterPack(opponent);
+      ] = monsterPack;
+    });
+
+    it('should register each created monster in static property uncoveredInstances', () => {
+      expect(Monster.uncoveredInstances).toEqual(monsterPack);
+    });
+
+    describe('clearUncoveredInstances method', () => {
+      it('should empty Monster.uncoveredInstances array', () => {
+        expect(Monster.uncoveredInstances.length).toBe(monsterPack.length);
+        Monster.clearUncoveredInstances();
+        expect(Monster.uncoveredInstances.length).toBe(0);
+      });
+    });
+
+    it('should register the number of instances of its type in nthOfItsType property', () => {
       expect(monsterType1_1.nthOfItsType).toBe(1);
       expect(monsterType2_1.nthOfItsType).toBe(1);
       expect(monsterType1_2.nthOfItsType).toBe(2);
       expect(monsterType2_2.nthOfItsType).toBe(2);
       expect(monsterType1_3.nthOfItsType).toBe(3);
-    });
-  });
-
-  describe('clearUncoveredInstances', () => {
-    it('should empty Monster.uncoveredInstances array', () => {
-      expect(Monster.uncoveredInstances.length).toBe(1);
-      Monster.clearUncoveredInstances();
-      expect(Monster.uncoveredInstances.length).toBe(0);
-      const monsterPackAmount = buildMonsterPack(opponent).length;
-      expect(Monster.uncoveredInstances.length).toBe(monsterPackAmount);
-      Monster.clearUncoveredInstances();
-      expect(Monster.uncoveredInstances.length).toBe(0);
     });
   });
 });
