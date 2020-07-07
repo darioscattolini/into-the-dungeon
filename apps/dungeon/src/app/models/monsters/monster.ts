65b14a1c17fb008a73873eb07a8939c01c4bf665
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-07 21:23:08 +0200

    feat<MonsterModel>: the amount of its type represented by each instance is stored in nthOfItsType field

--- apps/dungeon/src/app/models/monsters/monster.ts
+++ apps/dungeon/src/app/models/monsters/monster.ts
@@ -3,12 +3,17 @@ import { Hero } from '../heroes/hero';
 export abstract class Monster {
   public static uncoveredInstances: Monster[] = []; // this field should be private (or perhaps protected)
 
+  public nthOfItsType: number;  // this field should be protected
+
   constructor(
     public name: string,
     public baseDamage: number | null,
     public opponent: Hero   // this field should be protected
   ) {
     Monster.uncoveredInstances.push(this);
+    this.nthOfItsType = Monster.uncoveredInstances.filter(
+      monster => monster.constructor.name === this.constructor.name
+    ).length;
   }
 
   public static clearUncoveredInstances() {
