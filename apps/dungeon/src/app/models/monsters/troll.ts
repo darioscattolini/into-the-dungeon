441851073f7e3e447c08b7ad4b3a9ea0e7c5448f
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-14 21:41:48 +0200

    feat<TrollModel>: add static readonly property type with value 'Troll'

--- apps/dungeon/src/app/models/monsters/troll.ts
+++ apps/dungeon/src/app/models/monsters/troll.ts
@@ -2,6 +2,8 @@ import { Monster } from './monster';
 import { HeroInterface } from '../heroes/hero-interface';
 
 export class Troll extends Monster {
+  public static readonly type = 'Troll';
+
   constructor(opponent: HeroInterface) {
     super(opponent);
   }
