34b0d447558927631ef77416715215658e6f56a0
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-15 20:14:23 +0200

    test<VampireModel>: add spec for Vampire Model

--- apps/dungeon/src/app/models/monsters/vampire.spec.ts
+++ apps/dungeon/src/app/models/monsters/vampire.spec.ts
@@ -1,7 +1,44 @@
 import { Vampire } from './vampire';
+import { Monster } from './monster';
+import { HeroInterface } from '../heroes/hero-interface';
 
 describe('Vampire', () => {
+  let vampire: Vampire;
+  let opponent: HeroInterface;
+
+  beforeEach(() => {
+    opponent = {
+      equipment: [],
+      getDamageModifiers() { return { first: [], second: [] } }
+    }
+    vampire = new Vampire(opponent);
+  });
+
+  afterEach(() => {
+    Monster.clearUncoveredInstances();
+  });
+
+  it('should have static property maxAmount with value 2', () => {
+    expect(Vampire.maxAmount).toBe(2);
+  });
+
   it('should create an instance', () => {
-    expect(new Vampire()).toBeTruthy();
+    expect(vampire).toBeTruthy();
+  });
+
+  it('should create an instance of Vampire', () => {
+    expect(vampire instanceof Vampire).toBe(true);
+  });
+
+  it('should create an instance of Monster', () => {
+    expect(vampire instanceof Monster).toBe(true);
+  });
+  
+  it('should create an instance with type "Vampire"', () => {
+    expect(vampire.type).toBe('Vampire');
+  });
+
+  it('should create an instance with baseDamage of 4', () => {
+    expect(vampire.baseDamage).toBe(4);
   });
 });
