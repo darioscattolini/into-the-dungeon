da9ff27163c89cac6bb9d5a3144521181e0f807d
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-15 20:29:08 +0200

    test<DemonModel>: add spec for Demon Model

--- apps/dungeon/src/app/models/monsters/demon.spec.ts
+++ apps/dungeon/src/app/models/monsters/demon.spec.ts
@@ -1,7 +1,44 @@
 import { Demon } from './demon';
+import { Monster } from './monster';
+import { HeroInterface } from '../heroes/hero-interface';
 
-describe('Demon', () => {
+describe('Vampire', () => {
+  let demon: Demon;
+  let opponent: HeroInterface;
+
+  beforeEach(() => {
+    opponent = {
+      equipment: [],
+      getDamageModifiers() { return { first: [], second: [] } }
+    }
+    demon = new Demon(opponent);
+  });
+
+  afterEach(() => {
+    Monster.clearUncoveredInstances();
+  });
+
+  it('should have static property maxAmount with value 1', () => {
+    expect(Demon.maxAmount).toBe(1);
+  });
+
   it('should create an instance', () => {
-    expect(new Demon()).toBeTruthy();
+    expect(demon).toBeTruthy();
+  });
+
+  it('should create an instance of Demon', () => {
+    expect(demon instanceof Demon).toBe(true);
+  });
+
+  it('should create an instance of Monster', () => {
+    expect(demon instanceof Monster).toBe(true);
+  });
+  
+  it('should create an instance with type "Demon"', () => {
+    expect(demon.type).toBe('Demon');
+  });
+
+  it('should create an instance with baseDamage of 7', () => {
+    expect(demon.baseDamage).toBe(7);
   });
 });
