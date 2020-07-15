a06270f88521585fd84fdce463661417389b0c21
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-15 20:37:24 +0200

    feat<DragonModel>: add implementation for Dragon Model

--- apps/dungeon/src/app/models/monsters/dragon.ts
+++ apps/dungeon/src/app/models/monsters/dragon.ts
@@ -1,2 +1,13 @@
-export class Dragon {
-}
+import { Monster } from './monster';
+import { ConcreteMonsterStatic } from './concrete-monster-static';
+import { HeroInterface } from '../heroes/hero-interface';
+import { staticImplements } from '../../utilities';
+
+@staticImplements<ConcreteMonsterStatic>()
+export class Dragon extends Monster {
+  public static readonly maxAmount = 1;
+
+  constructor(opponent: HeroInterface) {
+    super('Dragon', 9, opponent);
+  }
+};
