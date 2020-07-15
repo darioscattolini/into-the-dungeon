7d6562da1ef7c8403e9bfeca2cffd89ba7edb81c
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-15 20:19:08 +0200

    test<GolemModel>: add spec for Golem Model

--- apps/dungeon/src/app/models/monsters/golem.spec.ts
+++ apps/dungeon/src/app/models/monsters/golem.spec.ts
@@ -1,7 +1,44 @@
 import { Golem } from './golem';
+import { Monster } from './monster';
+import { HeroInterface } from '../heroes/hero-interface';
 
-describe('Golem', () => {
+describe('Vampire', () => {
+  let golem: Golem;
+  let opponent: HeroInterface;
+
+  beforeEach(() => {
+    opponent = {
+      equipment: [],
+      getDamageModifiers() { return { first: [], second: [] } }
+    }
+    golem = new Golem(opponent);
+  });
+
+  afterEach(() => {
+    Monster.clearUncoveredInstances();
+  });
+
+  it('should have static property maxAmount with value 2', () => {
+    expect(Golem.maxAmount).toBe(2);
+  });
+
   it('should create an instance', () => {
-    expect(new Golem()).toBeTruthy();
+    expect(golem).toBeTruthy();
+  });
+
+  it('should create an instance of Golem', () => {
+    expect(golem instanceof Golem).toBe(true);
+  });
+
+  it('should create an instance of Monster', () => {
+    expect(golem instanceof Monster).toBe(true);
+  });
+  
+  it('should create an instance with type "Golem"', () => {
+    expect(golem.type).toBe('Golem');
+  });
+
+  it('should create an instance with baseDamage of 5', () => {
+    expect(golem.baseDamage).toBe(5);
   });
 });
