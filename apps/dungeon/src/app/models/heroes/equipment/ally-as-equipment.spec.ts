2c4e86481bba0038b2700a353883ecc9b61a164c
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-15 21:24:48 +0200

    feat<AllyAsEquipmentModel>: add AllyAsEquipment model

---
+++ apps/dungeon/src/app/models/heroes/equipment/ally-as-equipment.spec.ts
@@ -0,0 +1,7 @@
+import { AllyAsEquipment } from './ally-as-equipment';
+
+describe('AllyAsEquipment', () => {
+  it('should create an instance', () => {
+    expect(new AllyAsEquipment()).toBeTruthy();
+  });
+});
