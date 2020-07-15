51408673c9a024d4eae709d6124c839364bb0ea4
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-15 20:45:28 +0200

    test<FairyModel>: add spec for Fairy Model

--- apps/dungeon/src/app/models/monsters/fairy.spec.ts
+++ apps/dungeon/src/app/models/monsters/fairy.spec.ts
@@ -1,7 +1,44 @@
 import { Fairy } from './fairy';
+import { Monster } from './monster';
+import { HeroInterface } from '../heroes/hero-interface';
 
 describe('Fairy', () => {
+  let fairy: Fairy;
+  let opponent: HeroInterface;
+
+  beforeEach(() => {
+    opponent = {
+      equipment: [],
+      getDamageModifiers() { return { first: [], second: [] } }
+    }
+    fairy = new Fairy(opponent);
+  });
+
+  afterEach(() => {
+    Monster.clearUncoveredInstances();
+  });
+
+  it('should have static property maxAmount with value 1', () => {
+    expect(Fairy.maxAmount).toBe(1);
+  });
+
   it('should create an instance', () => {
-    expect(new Fairy()).toBeTruthy();
+    expect(fairy).toBeTruthy();
+  });
+
+  it('should create an instance of Fairy', () => {
+    expect(fairy instanceof Fairy).toBe(true);
+  });
+
+  it('should create an instance of Monster', () => {
+    expect(fairy instanceof Monster).toBe(true);
+  });
+  
+  it('should create an instance with type "Fairy"', () => {
+    expect(fairy.type).toBe('Fairy');
+  });
+
+  it('should create an instance with baseDamage of 0', () => {
+    expect(fairy.baseDamage).toBe(0);
   });
 });
