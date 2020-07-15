ca97938358ebd3797993687d42c4c0b60c925a50
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-15 20:04:57 +0200

    test<SkeletonModel>: add spec for Skeleton

---
+++ apps/dungeon/src/app/models/monsters/skeleton.spec.ts
@@ -0,0 +1,44 @@
+import { Skeleton } from './skeleton';
+import { Monster } from './monster';
+import { HeroInterface } from '../heroes/hero-interface';
+
+describe('Skeleton', () => {
+  let skeleton: Skeleton;
+  let opponent: HeroInterface;
+
+  beforeEach(() => {
+    opponent = {
+      equipment: [],
+      getDamageModifiers() { return { first: [], second: [] } }
+    }
+    skeleton = new Skeleton(opponent);
+  });
+
+  afterEach(() => {
+    Monster.clearUncoveredInstances();
+  });
+
+  it('should have static property maxAmount with value 2', () => {
+    expect(Skeleton.maxAmount).toBe(2);
+  });
+
+  it('should create an instance', () => {
+    expect(skeleton).toBeTruthy();
+  });
+
+  it('should create an instance of Skeleton', () => {
+    expect(skeleton instanceof Skeleton).toBe(true);
+  });
+
+  it('should create an instance of Monster', () => {
+    expect(skeleton instanceof Monster).toBe(true);
+  });
+  
+  it('should create an instance with type "Skeleton"', () => {
+    expect(skeleton.type).toBe('Skeleton');
+  });
+
+  it('should create an instance with baseDamage of 1', () => {
+    expect(skeleton.baseDamage).toBe(1);
+  });
+});
