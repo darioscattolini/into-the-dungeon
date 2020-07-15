f96b07151266c5877677e65ebfefd7154c447898
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-15 20:30:40 +0200

    feat<DemonModel>: add implementation for Demon Model

--- apps/dungeon/src/app/models/monsters/demon.ts
+++ apps/dungeon/src/app/models/monsters/demon.ts
@@ -1,2 +1,13 @@
-export class Demon {
-}
+import { Monster } from './monster';
+import { ConcreteMonsterStatic } from './concrete-monster-static';
+import { HeroInterface } from '../heroes/hero-interface';
+import { staticImplements } from '../../utilities';
+
+@staticImplements<ConcreteMonsterStatic>()
+export class Demon extends Monster {
+  public static readonly maxAmount = 1;
+
+  constructor(opponent: HeroInterface) {
+    super('Demon', 7, opponent);
+  }
+};
