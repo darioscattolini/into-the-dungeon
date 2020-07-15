08fe34efdbb61c872b6a360b61b1f8652fbe410e
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-15 21:23:20 +0200

    feat<EquipmentInterface>: extend BaseEquipment interface

--- apps/dungeon/src/app/models/heroes/equipment/equipment-interface.ts
+++ apps/dungeon/src/app/models/heroes/equipment/equipment-interface.ts
@@ -2,7 +2,10 @@ import { Monster } from '../../monsters/monster';
 
 interface BaseEquipment {
   name: string;
+  available: boolean; // discarded or not
   modifiesDamage: boolean;
+  canBeUsedAgainst? (monster: Monster): boolean;
+  useAgainst? (monster: Monster): void;
 }
 
 export type DamageModifier = (baseDamage: number) => number;
