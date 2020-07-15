083eb1eb6eafcea2335e10f44ee9a48e2ea722f6
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-15 21:09:18 +0200

    test<AllyModel>: add basic spec for Ally Model

--- apps/dungeon/src/app/models/monsters/ally.spec.ts
+++ apps/dungeon/src/app/models/monsters/ally.spec.ts
@@ -1,7 +1,44 @@
 import { Ally } from './ally';
+import { Monster } from './monster';
+import { HeroInterface } from '../heroes/hero-interface';
 
 describe('Ally', () => {
+  let ally: Ally;
+  let opponent: HeroInterface;
+
+  beforeEach(() => {
+    opponent = {
+      equipment: [],
+      getDamageModifiers() { return { first: [], second: [] } }
+    };
+    ally = new Ally(opponent);
+  });
+
+  afterEach(() => {
+    Monster.clearUncoveredInstances();
+  });
+
+  it('should have static property maxAmount with value 1', () => {
+    expect(Ally.maxAmount).toBe(1);
+  });
+
   it('should create an instance', () => {
-    expect(new Ally()).toBeTruthy();
+    expect(ally).toBeTruthy();
+  });
+
+  it('should create an instance of Ally', () => {
+    expect(ally instanceof Ally).toBe(true);
+  });
+
+  it('should create an instance of Monster', () => {
+    expect(ally instanceof Monster).toBe(true);
+  });
+  
+  it('should create an instance with type "Ally"', () => {
+    expect(ally.type).toBe('Ally');
+  });
+
+  it('should create an instance with baseDamage of null', () => {
+    expect(ally.baseDamage).toBe(null);
   });
 });
