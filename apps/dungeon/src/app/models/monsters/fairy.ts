55cee2499797b6f6b277ae063b7ecd4919ccdfa5
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-15 20:46:34 +0200

    feat<FairyModel>: add basic implementation for Fairy Model

--- apps/dungeon/src/app/models/monsters/fairy.ts
+++ apps/dungeon/src/app/models/monsters/fairy.ts
@@ -1,2 +1,13 @@
-export class Fairy {
-}
+import { Monster } from './monster';
+import { ConcreteMonsterStatic } from './concrete-monster-static';
+import { HeroInterface } from '../heroes/hero-interface';
+import { staticImplements } from '../../utilities';
+
+@staticImplements<ConcreteMonsterStatic>()
+export class Fairy extends Monster {
+  public static readonly maxAmount = 1;
+
+  constructor(opponent: HeroInterface) {
+    super('Fairy', 0, opponent);
+  }
+};
