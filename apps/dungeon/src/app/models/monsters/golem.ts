52ed3cd513e3fde42b8ecb28e9367356cb9f6233
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-15 20:20:36 +0200

    feat<GolemModel>: add implementation for Golem Model

--- apps/dungeon/src/app/models/monsters/golem.ts
+++ apps/dungeon/src/app/models/monsters/golem.ts
@@ -1,2 +1,13 @@
-export class Golem {
-}
+import { Monster } from './monster';
+import { ConcreteMonsterStatic } from './concrete-monster-static';
+import { HeroInterface } from '../heroes/hero-interface';
+import { staticImplements } from '../../utilities';
+
+@staticImplements<ConcreteMonsterStatic>()
+export class Golem extends Monster {
+  public static readonly maxAmount = 2;
+
+  constructor(opponent: HeroInterface) {
+    super('Golem', 5, opponent);
+  }
+};
