7ac1ab1a0b03db16aa4cf8fd0c0dc6871d014432
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-15 20:15:19 +0200

    feat<VampireModel>: add implementation for Vampire Model

--- apps/dungeon/src/app/models/monsters/vampire.ts
+++ apps/dungeon/src/app/models/monsters/vampire.ts
@@ -1,2 +1,13 @@
-export class Vampire {
-}
+import { Monster } from './monster';
+import { ConcreteMonsterStatic } from './concrete-monster-static';
+import { HeroInterface } from '../heroes/hero-interface';
+import { staticImplements } from '../../utilities';
+
+@staticImplements<ConcreteMonsterStatic>()
+export class Vampire extends Monster {
+  public static readonly maxAmount = 2;
+
+  constructor(opponent: HeroInterface) {
+    super('Vampire', 4, opponent);
+  }
+};
