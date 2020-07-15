d22f912dd46bbd331fccabb3bd1724ef35e919ce
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-15 21:10:19 +0200

    feat<AllyModel>: add basic implementation for Ally Model

--- apps/dungeon/src/app/models/monsters/ally.ts
+++ apps/dungeon/src/app/models/monsters/ally.ts
@@ -1,2 +1,13 @@
-export class Ally {
-}
+import { Monster } from './monster';
+import { ConcreteMonsterStatic } from './concrete-monster-static';
+import { HeroInterface } from '../heroes/hero-interface';
+import { staticImplements } from '../../utilities';
+
+@staticImplements<ConcreteMonsterStatic>()
+export class Ally extends Monster {
+  public static readonly maxAmount = 1;
+
+  constructor(opponent: HeroInterface) {
+    super('Ally', null, opponent);
+  }
+};
