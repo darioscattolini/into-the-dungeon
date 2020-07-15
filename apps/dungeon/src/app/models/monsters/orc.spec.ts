096235a7f7534692a0359858118a2dbfdb34bc82
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-15 20:10:07 +0200

    test<OrcModel>: add spec for Orc

--- apps/dungeon/src/app/models/monsters/orc.spec.ts
+++ apps/dungeon/src/app/models/monsters/orc.spec.ts
@@ -1,7 +1,44 @@
 import { Orc } from './orc';
+import { Monster } from './monster';
+import { HeroInterface } from '../heroes/hero-interface';
 
-describe('Orc', () => {
+describe('Skeleton', () => {
+  let orc: Orc;
+  let opponent: HeroInterface;
+
+  beforeEach(() => {
+    opponent = {
+      equipment: [],
+      getDamageModifiers() { return { first: [], second: [] } }
+    }
+    orc = new Orc(opponent);
+  });
+
+  afterEach(() => {
+    Monster.clearUncoveredInstances();
+  });
+
+  it('should have static property maxAmount with value 2', () => {
+    expect(Orc.maxAmount).toBe(2);
+  });
+
   it('should create an instance', () => {
-    expect(new Orc()).toBeTruthy();
+    expect(orc).toBeTruthy();
+  });
+
+  it('should create an instance of Orc', () => {
+    expect(orc instanceof Orc).toBe(true);
+  });
+
+  it('should create an instance of Monster', () => {
+    expect(orc instanceof Monster).toBe(true);
+  });
+  
+  it('should create an instance with type "Orc"', () => {
+    expect(orc.type).toBe('Orc');
+  });
+
+  it('should create an instance with baseDamage of 3', () => {
+    expect(orc.baseDamage).toBe(3);
   });
 });
