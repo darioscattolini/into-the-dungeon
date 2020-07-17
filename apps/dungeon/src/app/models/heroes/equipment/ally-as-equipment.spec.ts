ba03357abe5c4c4ab3dcca672a91fb7b919fbe2e
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-17 16:55:02 +0200

    test<AllyAsEquipmentModel>: add spec for canBeUsedAgainst method

--- apps/dungeon/src/app/models/heroes/equipment/ally-as-equipment.spec.ts
+++ apps/dungeon/src/app/models/heroes/equipment/ally-as-equipment.spec.ts
@@ -1,10 +1,25 @@
 import { AllyAsEquipment } from './ally-as-equipment';
+import { HeroInterface } from '../hero-interface';
+import { Monster } from '../../monsters/monster';
 
 describe('AllyAsEquipment', () => {
   let ally: AllyAsEquipment;
+  let allysPosition: number;
+  let opponent: HeroInterface;
 
   beforeEach(() => {
-    ally = new AllyAsEquipment();
+    allysPosition = 2;
+    ally = new AllyAsEquipment(allysPosition);
+    opponent = {
+      equipment: [],
+      getDamageModifiers() {
+        return {
+          first: [],
+          second: []
+        }
+      }
+    };
+    Monster.clearUncoveredInstances();
   });
   
   it('should create an instance', () => {
@@ -25,5 +40,17 @@ describe('AllyAsEquipment', () => {
 
   it('should create an instance with field modifiesDamage with value false', () => {
     expect(ally.modifiesDamage).toBe(false);
+  });
+
+  it('can only be used against ally\'s next monster', () => {
+    const monster1 = new class extends Monster {} ('Troll', 1, opponent);
+    const monster2 = new class extends Monster {} ('Ally', null, opponent);
+    const monster3 = new class extends Monster {} ('Troll', 1, opponent);
+    const monster4 = new class extends Monster {} ('Orc', 3, opponent);
+    
+    expect(ally.canBeUsedAgainst(monster1)).toBe(false);
+    expect(ally.canBeUsedAgainst(monster2)).toBe(false);
+    expect(ally.canBeUsedAgainst(monster3)).toBe(true);
+    expect(ally.canBeUsedAgainst(monster4)).toBe(false);
   });
 });
