1252bf9086d7bcb756ca1228892cdd81704ef442
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-17 17:17:08 +0200

    feat<AllyAsEquipmentModel>: implement canBeUsedAgainst method

--- apps/dungeon/src/app/models/heroes/equipment/ally-as-equipment.ts
+++ apps/dungeon/src/app/models/heroes/equipment/ally-as-equipment.ts
@@ -1,5 +1,20 @@
+import { EquipmentWeapon } from './equipment-interface';
+import { Monster } from '../../monsters/monster';
+
 export class AllyAsEquipment {
   public readonly name = 'Ally';
   public readonly modifiesDamage = false;
   public available = true;
+  private positionInDungeon: number;
+
+  constructor(positionInDungeon: number) {
+    this.positionInDungeon = positionInDungeon;
+  }
+
+  public canBeUsedAgainst(monster: Monster) {
+    return monster.positionInDungeon === this.positionInDungeon + 1;
+  }
 }
+
+// canBeUsedAgainst? (monster: Monster): boolean; if monster is of nth type. calls discard method conditionally in case it is not used in previous monster
+// useAgainst? (monster: Monster): void; calls discard method after use
