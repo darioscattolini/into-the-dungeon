77a3a01b60437568c88e8a57d2ad4cf1eb1eaa18
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-25 22:20:38 +0200

    refactor<HeroModel> remove getDamageModifiers method

--- apps/dungeon/src/app/models/hero/hero.ts
+++ apps/dungeon/src/app/models/hero/hero.ts
@@ -1,19 +1,7 @@
 import { IEquipment } from '../equipment/equipment.interface';
-import { DamageModifier } from '../equipment/equipment.interface';
-
-interface IDamageModifierMessage {
-  first: DamageModifier[],
-  second: DamageModifier[]
-}
 
 export abstract class Hero {
   
   constructor(public equipment: IEquipment[]) { }
 
-  getDamageModifiers(): IDamageModifierMessage {
-    return {
-      first: [],
-      second: []
-    }
-  }
 }
