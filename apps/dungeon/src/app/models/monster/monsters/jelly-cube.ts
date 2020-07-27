595425c5fc59fd5c2e567eca78623b2eddd52acd
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-27 21:37:12 +0200

    feat<JellyCube>: implement JellyCube class

--- apps/dungeon/src/app/models/monster/monsters/jelly-cube.ts
+++ apps/dungeon/src/app/models/monster/monsters/jelly-cube.ts
@@ -1,2 +1,32 @@
-export class JellyCube {
-}
+import { Monster } from '../monster';
+import { IDerivedMonsterStatic } from '../derived-monster-static.interface';
+import { staticImplements } from '../../../utilities';
+import { RareMonsterType } from '../rare-monster-type';
+import { ILoseEquipmentEffect } from '../../models';
+
+@staticImplements<IDerivedMonsterStatic>()
+export class JellyCube extends Monster {
+  public static readonly maxAmount = 1;
+  
+  protected readonly _type: RareMonsterType = 'Jelly Cube';
+  protected readonly _baseDamage = null;
+  
+  public get type() {
+    return this._type;
+  }
+
+  public get baseDamage() {
+    return this._baseDamage;
+  }
+
+  public produceEffect(): ILoseEquipmentEffect {
+    return {
+      type: 'equipment',
+      lose: 'any'
+    }
+  }
+
+  public startingAction(): null {
+    return null;
+  }
+};
