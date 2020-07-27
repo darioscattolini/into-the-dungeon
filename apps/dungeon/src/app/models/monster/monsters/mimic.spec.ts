e33705b71094e6c6ed71aef387380cab626f008c
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-27 20:26:26 +0200

    test<Monster: Mimic>: add spec for Mimic monster

---
+++ apps/dungeon/src/app/models/monster/monsters/mimic.spec.ts
@@ -0,0 +1,83 @@
+import { Mimic } from './mimic';
+import { Monster } from '../monster';
+import { ITransformationEffect } from '../../models';
+import { noEquipHeroStub, oneEquipHeroStub } from '../../mocks';
+
+describe('Mimic', () => {
+  let mimic: Mimic;
+
+  beforeEach(() => {
+    mimic = new Mimic();
+  });
+
+  it('should have static property maxAmount with value 1', () => {
+    expect(Mimic.maxAmount).toBe(1);
+  });
+
+  it('should create an instance', () => {
+    expect(mimic).toBeTruthy();
+  });
+
+  it('should create an instance of Mimic', () => {
+    expect(mimic instanceof Mimic).toBe(true);
+  });
+
+  it('should create an instance of Monster', () => {
+    expect(mimic instanceof Monster).toBe(true);
+  });
+  
+  it('should create an instance with type "Mimic"', () => {
+    expect(mimic.type).toBe('Mimic');
+  });
+
+  it('should create an instance with baseDamage of null', () => {
+    expect(mimic.baseDamage).toBe(null);
+  });
+
+  describe('startingAction', () => {
+    const startingAction: ITransformationEffect = mimic.startingAction();
+    
+    it('should be nonnull', () => {
+      expect(startingAction).not.toBeNull();
+    });
+  
+    it('should be of transformation type', () => {
+      expect(startingAction.type).toBe('transformation');
+    });
+
+    it('should specify hero as parameter for transformer function', () => {
+      expect(startingAction.parameter).toBe('hero');
+    });
+
+    it('should specify a transformer function', () => {
+      expect(typeof startingAction.transformer).toBe('function');
+    });
+
+    describe('transformer function', () => {
+      const transform = startingAction.transformer;
+      const noEquipMimic = transform(noEquipHeroStub);
+      const oneEquipMimic = transform(oneEquipHeroStub);
+
+      it('should return a Mimic', () => {
+        expect(noEquipMimic instanceof Mimic).toBe(true);
+        expect(oneEquipMimic instanceof Mimic).toBe(true);
+      });
+
+      it('should return a Mimic of baseDamage 0 for hero with no equipment', () => {
+        expect(noEquipMimic.baseDamage).toBe(0);
+      });
+
+      it('should return a Mimic of actualDamage 0 for hero with no equipment', () => {
+        expect(noEquipMimic.actualDamage).toBe(0);
+      });
+
+      it('should return a Mimic of baseDamage 1 for hero with 1 equipment', () => {
+        expect(oneEquipMimic.baseDamage).toBe(1);
+      });
+
+      it('should return a Mimic of actualDamage 1 for hero with 1 equipment', () => {
+        expect(oneEquipMimic.actualDamage).toBe(1);
+      });
+    });
+  });
+});
