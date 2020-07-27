5eb23c65fdd6b87840938ea62146f362927ee474
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-27 21:08:08 +0200

    feat<Monster: Mimic>: make Mimic instance of DerivedMonsterStatic

--- apps/dungeon/src/app/models/monster/monsters/mimic.ts
+++ apps/dungeon/src/app/models/monster/monsters/mimic.ts
@@ -1,7 +1,12 @@
 import { Monster, ITransformationEffect, IDamageEffect, Hero } from '../../models';
 import { RareMonsterType } from '../rare-monster-type';
+import { staticImplements } from '../../../utilities';
+import { IDerivedMonsterStatic } from '../derived-monster-static.interface';
 
+@staticImplements<IDerivedMonsterStatic>()
 export class Mimic extends Monster {
+  public static readonly maxAmount = 1;
+  
   protected _type: RareMonsterType = 'Mimic';
   protected _baseDamage: number | null = null;
 
