14b5495d9794d7bbb38740da5a5ea6587d2f18d7
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-28 20:30:41 +0200

    test<HeroesService>: add spec for getHero

--- apps/dungeon/src/app/one-device/services/heroes.service.spec.ts
+++ apps/dungeon/src/app/one-device/services/heroes.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HeroesService } from './heroes.service';
-import { heroes, IHero } from '../../models/models';
+import { heroes, IHero, Bard, Mage, Ninja, Princess } from '../../models/models';
 
 describe('HeroesService', () => {
   let service: HeroesService;
@@ -30,6 +30,24 @@ describe('HeroesService', () => {
       expect(service.getHeroesUIData()).toHaveLength(heroesUIData.length);
       expect(service.getHeroesUIData())
         .toEqual(expect.arrayContaining(heroesUIData));
+    });
+  });
+
+  describe('getHero', () => {
+    it('should return an instance of Bard if called with "Bard"', () => {
+      expect(service.getHero('Bard') instanceof Bard).toBe(true)
+    });
+
+    it('should return an instance of Mage if called with "Mage"', () => {
+      expect(service.getHero('Mage') instanceof Mage).toBe(true)
+    });
+
+    it('should return an instance of Ninja if called with "Ninja"', () => {
+      expect(service.getHero('Ninja') instanceof Ninja).toBe(true)
+    });
+
+    it('should return an instance of Princess if called with "Princess"', () => {
+      expect(service.getHero('Princess') instanceof Princess).toBe(true)
     });
   });
 });
