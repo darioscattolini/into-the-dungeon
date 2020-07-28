0e0fca4f4871274cca107c81fcfde66a425f40ec
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-28 20:35:06 +0200

    feat<HeroesService>: implement getHero

--- apps/dungeon/src/app/one-device/services/heroes.service.ts
+++ apps/dungeon/src/app/one-device/services/heroes.service.ts
@@ -1,23 +1,27 @@
 import { Injectable } from '@angular/core';
 
 import { OneDeviceModule } from '../one-device.module';
-import { Bard, Mage, Ninja, Princess, IDerivedHeroStatic, IHero, heroes } from '../../models/models';
+import { Bard, Mage, Ninja, Princess, IDerivedHeroStatic, IHero, heroes, Hero } from '../../models/models';
 
 @Injectable({
   providedIn: OneDeviceModule
 })
 export class HeroesService {
-  private heroClasses: IDerivedHeroStatic[] = [
-    Bard,
-    Mage,
-    Ninja,
-    Princess
-  ];
+  private constructors = {
+    Bard: Bard,
+    Mage: Mage,
+    Ninja: Ninja,
+    Princess: Princess
+  }
   private heroesUIData = heroes;
 
   constructor() { }
 
   public getHeroesUIData(): IHero[] {
     return this.heroesUIData.slice(0);
+  }
+
+  public getHero(heroName: 'Bard' | 'Mage' | 'Ninja' | 'Princess'): Hero {
+    return new this.constructors[heroName]();
   }
 }
