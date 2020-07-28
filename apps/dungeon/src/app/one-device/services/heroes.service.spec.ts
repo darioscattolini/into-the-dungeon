621dc4c9d67f052d27cef5f735dabb8121f0d93e
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-28 14:25:06 +0200

    test<HeroesService>: add spec for getHeroes method

--- apps/dungeon/src/app/one-device/services/heroes.service.spec.ts
+++ apps/dungeon/src/app/one-device/services/heroes.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HeroesService } from './heroes.service';
+import { Bard, Princess, Mage, Ninja } from '../../models/models';
 
 describe('HeroesService', () => {
   let service: HeroesService;
@@ -11,10 +12,32 @@ describe('HeroesService', () => {
     });
     service = TestBed.inject(HeroesService);
   });
-
+  
   describe('constructor', () => {
     it('should create service', () => {
       expect(service).toBeTruthy();
+    });
+  });
+
+  describe('getHeroes', () => {
+    it('should retrieve an array with four members', () => {
+      expect(service.getHeroes()).toHaveLength(4);
+    });
+
+    it('should return an array containing Bard class', () => {
+      expect(service.getHeroes()).toContain(Bard);
+    });
+
+    it('should return an array containin Mage class', () => {
+      expect(service.getHeroes()).toContain(Mage);
+    });
+
+    it('should return an array containin Ninja class', () => {
+      expect(service.getHeroes()).toContain(Ninja);
+    });
+    
+    it('should return an array containing Princess class', () => {
+      expect(service.getHeroes()).toContain(Princess);
     });
   });
 });
