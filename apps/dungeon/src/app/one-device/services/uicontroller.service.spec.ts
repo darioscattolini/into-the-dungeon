cfe26d3346edf062e1fa7f4fbaa6b537f9958dc8
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-28 17:54:01 +0200

    test<UIService>: add basic spec for requestChoice method

--- apps/dungeon/src/app/one-device/services/uicontroller.service.spec.ts
+++ apps/dungeon/src/app/one-device/services/uicontroller.service.spec.ts
@@ -13,4 +13,34 @@ describe('UIControllerService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('requestChoice', () => {
+    it('shoud throw error with no parameter', () => {
+      expect(service.requestChoice())
+        .toThrow('requestChoice must be called with a ChoiceRequest parameter');
+    });
+
+    it('shoud throw error with empty parameter', () => {
+      expect(service.requestChoice({}))
+        .toThrow('requestChoice must be called with a ChoiceRequest parameter');
+    });
+
+    it('shoud throw error with incomplete parameter', () => {
+      expect(service.requestChoice({player: 'player'}))
+        .toThrow('requestChoice must be called with a ChoiceRequest parameter');
+    });
+
+    it('shoud throw error with incomplete parameter', () => {
+      expect(service.requestChoice({options: ['one', 'two']}))
+        .toThrow('requestChoice must be called with a ChoiceRequest parameter');
+    });
+
+    it('shoud throw error with only one option', () => {
+      expect(service.requestAction({player: 'player', options: ['one']}))
+        .toThrow('requestChoice must be called with a ChoiceRequest parameter');
+    });
+
+    // it should wait for user choice
+    // it shoud return a ChoiceResponse
+  });
 });
