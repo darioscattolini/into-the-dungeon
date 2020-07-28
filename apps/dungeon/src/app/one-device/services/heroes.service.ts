e36a06e34d94257a49731b215bcd869a34a2b732
Darío Scattolini <dario.scattolini@gmail.com>
2020-07-28 14:37:25 +0200

    test<HeroesService>: implement getHeroes method

--- apps/dungeon/src/app/one-device/services/heroes.service.ts
+++ apps/dungeon/src/app/one-device/services/heroes.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
 
 import { OneDeviceModule } from '../one-device.module';
+import { Bard, Mage, Ninja, Princess, IDerivedHeroStatic } from '../../models/models';
 
 @Injectable({
   providedIn: OneDeviceModule
 })
 export class HeroesService {
+  private heroes: IDerivedHeroStatic[] = [
+    Bard,
+    Mage,
+    Ninja,
+    Princess
+  ];
 
   constructor() { }
+
+  public getHeroes(): IDerivedHeroStatic[] {
+    return this.heroes.slice(0);
+  }
 }
