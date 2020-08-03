05be5dd9917808f48d6860ac41357f3fb0fdf362
Darío Scattolini <dario.scattolini@gmail.com>
2020-08-03 13:51:50 +0200

    test<MonstersService>: add basic spec for getMonstersMace method

--- apps/dungeon/src/app/one-device/services/monsters.service.spec.ts
+++ apps/dungeon/src/app/one-device/services/monsters.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MonstersService } from './monsters.service';
+import { Monster } from '../../models/models';
 
 describe('MonstersService', () => {
   let service: MonstersService;
@@ -14,5 +15,22 @@ describe('MonstersService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getMonstersMace', () => {
+    let monstersMace: Monster[];
+    
+    beforeEach(() => {
+      monstersMace = service.getMonstersMace();
+    });
+
+    it('should return array of 15 members', () => {
+      expect(monstersMace).toHaveLength(15);
+    });
+
+    it('should return array of monsters', () => {
+      expect(monstersMace.every(monster => monster instanceof Monster))
+        .toBe(true);
+    });
   });
 });
