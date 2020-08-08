00fa2f29e52bcf95ecb3fb82b6e0979c1be6b971
Darío Scattolini <dario.scattolini@gmail.com>
2020-08-08 19:27:36 +0200

    test<PlayersService>: refactor spec

--- apps/dungeon/src/app/one-device/services/players.service.spec.ts
+++ apps/dungeon/src/app/one-device/services/players.service.spec.ts
@@ -17,79 +17,86 @@ describe('PlayersService', () => {
     service = TestBed.inject(PlayersService);
   });
 
-  it('should be created', () => {
+  test('should be created', () => {
     expect(service).toBeTruthy();
   });
 
   describe('getAmountOfPlayers', () => {
-    it('should return 0 when service is created', () => {
+    test('it returns 0 just after service is created', () => {
       expect(service.getAmountOfPlayers()).toBe(0);
     });
 
-    it('should return 2 after 2 players are added', () => {
+    test('it returns 2 after 2 players are added', () => {
       addPlayers(service, ['John', 'Anna']);
       expect(service.getAmountOfPlayers()).toBe(2);
     });
   });
 
   describe('addPlayer', () => {
-    it('should add something to the list"', () => {
-      const emptyList = service.getPlayersList();
-      expect(emptyList).toHaveLength(0);
+    let listBefore: Player[];
 
-      service.addPlayer('John');
-      const listWithJohn = service.getPlayersList();
-      expect(listWithJohn).toHaveLength(1);
+    beforeEach(() => {
+      listBefore = service.getPlayersList();
     });
 
-    it('should add an instance of Player to the list"', () => {
+    test('it adds something to players list"', () => {
+      expect(listBefore).toHaveLength(0);
+
       service.addPlayer('John');
-      const listWithJohn = service.getPlayersList();
-      expect(listWithJohn[0]).toBeInstanceOf(Player);
+      const listAfter = service.getPlayersList();
+
+      expect(listAfter).toHaveLength(1);
     });
 
-    it('should add a player named John to the list"', () => {
+    test('it adds an instance of Player to the list"', () => {
+      service.addPlayer('John');
+      const listAfter = service.getPlayersList();
+
+      expect(listAfter[0]).toBeInstanceOf(Player);
+    });
+
+    test('it adds a player named after parameter to the list"', () => {
       service.addPlayer('John');
       const listWithJohn = service.getPlayersList();
+
       expect(listWithJohn[0].name).toBe('John');
     });
 
-    it('should return an instance of Player', () => {
+    test('it returns added Player', () => {
       const returnedPlayer = service.addPlayer('John');
-      expect(returnedPlayer).toBeInstanceOf(Player);
+      const addedPlayer = service.getPlayersList()[0];
+
+      expect(returnedPlayer).toBe(addedPlayer);
     });
 
-    it('should return a player named John', () => {
-      const returnedPlayer = service.addPlayer('John');
-      expect(returnedPlayer.name).toStrictEqual('John');
-    });
-
-    it('should allow to add up to 4 players', () => {
+    test('it allows to add up to 4 players', () => {
       const john = service.addPlayer('John');
       const anna = service.addPlayer('Anna');
       const chris = service.addPlayer('Chris');
       const teffy = service.addPlayer('Teffy');
-      expect(service.getPlayersList()).toEqual([john, anna, chris, teffy]);
+
+      expect(service.getPlayersList())
+        .toIncludeSameMembers([john, anna, chris, teffy]);
     });
 
-    it('should add no more than 4 players', () => {
+    test('it does not allow to add more than 4 players', () => {
       for (let i = 0; i < 4; i++) {
         service.addPlayer(String(i));
       }
 
-      expect(service.getAmountOfPlayers()).toStrictEqual(4);
+      expect(service.getAmountOfPlayers()).toBe(4);
 
       expect(() => { service.addPlayer('fifth'); })
         .toThrowError('There can only be four players in this game');
     });
 
-    it('should not allow players with the same name', () => {
+    test('it does not allow players with the same name', () => {
       service.addPlayer('John');
       expect(() => { service.addPlayer('John'); })
         .toThrowError('There can only be one player named John');
     });
 
-    it('should order players in round using player.nextPlayer field', () => {
+    test('it orders players in round using player.nextPlayer field', () => {
       const first = service.addPlayer('first');
       const second = service.addPlayer('second');
       expect(first.nextPlayer).toBe(second);
@@ -106,72 +113,70 @@ describe('PlayersService', () => {
   });
 
   describe('getPlayersList', () => {
-    it('should return a list of added players', () => {
+    test('it returns a list of added players', () => {
       const john = service.addPlayer('John');
       const anna = service.addPlayer('Anna');
-      expect(service.getPlayersList()).toEqual([john, anna]);
+      expect(service.getPlayersList())
+        .toIncludeSameMembers([john, anna]);
     });
 
-    it('should return a copy of its private list of players', () => {
+    test('should return a copy of its private list of players', () => {
       const john = service.addPlayer('John');
       const returnedList = service.getPlayersList();
       const anna = service.addPlayer('Anna');
       
-      expect(returnedList).toEqual([john]);
-      expect(service.getPlayersList()).toEqual([john, anna]);
+      expect(returnedList).toIncludeSameMembers([john]);
+      expect(service.getPlayersList()).toIncludeSameMembers([john, anna]);
     });
   });
 
   describe('getRandomPlayer', () => {
-    it('should throw error if called with no players', () => {
+    test('it throws error if no players were added', () => {
       expect(() => service.getRandomPlayer())
         .toThrowError('There are no players');
     });
 
-    it('should return an instance of Player', () => {
+    test('it returns a player from its list', () => {
       addPlayers(service, ['John', 'Anna', 'Chris', 'Julia']);
-      const randomPlayer = service.getRandomPlayer();
-      expect(randomPlayer instanceof Player).toBe(true);
-    });
+      const [ added1, added2, added3, added4 ] = service.getPlayersList();
 
-    it('should only return players from its list', () => {
-      addPlayers(service, ['John', 'Anna', 'Chris', 'Julia']);
       let unexpected = 0;
       for (let i = 0; i < 100; i++) {
         const randomPlayer = service.getRandomPlayer();
-        switch(randomPlayer.name) {
-          case 'John':
-          case 'Anna':
-          case 'Chris':
-          case 'Julia':
+        switch(randomPlayer) {
+          case added1:
+          case added2:
+          case added3:
+          case added4:
             break;
           default:
             unexpected++;
         }
+
         expect(unexpected).toBe(0);
       }
     });
 
-    it('should return all players given enough calls', () => {
+    test('it returns all players from its list given enough calls', () => {
       addPlayers(service, ['John', 'Anna', 'Chris', 'Julia']);
+
       const count = {
         John: 0,
         Anna: 0,
         Chris: 0,
-        Julia: 0
+        Julia: 0,
+        ready() {
+          return this.John > 0 && this.Anna > 0 && this.Chris > 0 &&
+                 this.Julia > 0;
+        }
       }
-      while(
-        count.John === 0 ||
-        count.Anna === 0 ||
-        count.Chris === 0 ||
-        count.Julia === 0
-      ) {
+      while(!count.ready()) {
         const randomPlayer = service.getRandomPlayer();
         const name = randomPlayer.name as 'John' | 'Anna' | 'Chris' | 'Julia';
         count[name]++;
       }
-      const allHaveBeenReturned = true;
-      expect(allHaveBeenReturned).toBe(true);
+      
+      expect(count.ready()).toBeTrue();
     });
 
     // It would be great to test that players are returned randomly,
@@ -189,40 +194,40 @@ describe('PlayersService', () => {
       [john, anna, chris, teffy] = service.getPlayersList();
     });
 
-    it ('should return false with 0 victories and defeats', () => {
-      expect(service.isThereAWinner()).toBe(false);
+    test('it returns false with 0 victories and defeats', () => {
+      expect(service.isThereAWinner()).toBeFalse();
     });
 
-    it ('should return false with some players with 1 victory', () => {
+    test('it returns false with some players with 1 victory', () => {
       john.surviveDungeon();
       chris.surviveDungeon();
-      expect(service.isThereAWinner()).toBe(false);
+      expect(service.isThereAWinner()).toBeFalse();
     });
 
-    it ('should return false with some players with 1 defeat', () => {
+    test('it returns false with some players with 1 defeat', () => {
       john.dieInDungeon();
       chris.dieInDungeon();
-      expect(service.isThereAWinner()).toBe(false);
+      expect(service.isThereAWinner()).toBeFalse();
     });
 
-    it ('should return false with mixes of 1 defeat/1 victory', () => {
-      john.dieInDungeon();
-      anna.surviveDungeon();
-      chris.dieInDungeon();
-      chris.surviveDungeon();
-      expect(service.isThereAWinner()).toBe(false);
-    });
-
-    it('should return true when 1 player gets 2 victories', () => {
+    test('it returns false with mixes of 1 defeat/1 victory', () => {
       john.dieInDungeon();
       anna.surviveDungeon();
       chris.dieInDungeon();
       chris.surviveDungeon();
-      chris.surviveDungeon();
-      expect(service.isThereAWinner()).toBe(true);
+      expect(service.isThereAWinner()).toBeFalse();
     });
 
-    it('should return true when all but 1 player are out of the game', () => {
+    test('it returns true when 1 player gets 2 victories', () => {
+      john.dieInDungeon();
+      anna.surviveDungeon();
+      chris.dieInDungeon();
+      chris.surviveDungeon();
+      chris.surviveDungeon();
+      expect(service.isThereAWinner()).toBeTrue();
+    });
+
+    test('it returns true when all but 1 player are out of the game', () => {
       john.dieInDungeon();
       chris.surviveDungeon();
       anna.dieInDungeon();
@@ -231,9 +236,9 @@ describe('PlayersService', () => {
       teffy.dieInDungeon();
       chris.dieInDungeon();
       john.dieInDungeon();
-      expect(service.isThereAWinner()).toBe(false);
+      expect(service.isThereAWinner()).toBeFalse();
       teffy.dieInDungeon();
-      expect(service.isThereAWinner()).toBe(true);
+      expect(service.isThereAWinner()).toBeTrue();
     });
   });
 
@@ -244,18 +249,24 @@ describe('PlayersService', () => {
     beforeEach(() => {
       john = service.addPlayer('John');
       anna = service.addPlayer('Anna');
-
     });
 
-    it('should throw error if there are no winners', () => {
+    test('it throws error if there are no winners', () => {
+      expect(service.isThereAWinner()).toBeFalse();
       expect(() => { service.getWinner() })
         .toThrowError('There is no winner yet');
     });
 
-    it('should return a winning player', () => {
+    test('it returns a 2-victory winning player', () => {
       anna.surviveDungeon();
       anna.surviveDungeon();
       expect(service.getWinner()).toBe(anna);
+    });
+
+    test('it returns a last-standing winning player', () => {
+      anna.dieInDungeon();
+      anna.dieInDungeon();
+      expect(service.getWinner()).toBe(john);
     });
   });
 });
